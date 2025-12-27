eabf16e189a74ac22b344d4cca890608897a7f35
Angel <angelin09ozoz@gmail.com>
2025-12-26 19:17:51 -0600

    Animated container animado

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -29,5 +29,10 @@ final appRouter = GoRouter(
       name: SnackBarScreen.name,
       builder: (context, state) => SnackBarScreen(),
     ),
+    GoRoute(
+      path: '/animated',
+      name: AnimatedScreen.name,
+      builder: (context, state) => AnimatedScreen(),
+    ),
   ],
 );
